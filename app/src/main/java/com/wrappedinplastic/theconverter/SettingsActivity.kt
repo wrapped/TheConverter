97b514064d3c9238b0ec2626dbe1f858b67de4d3
wrapped <ddroidx@gmail.com>
2020-02-01 11:01:29 -0600

    Introduce dark theme option

--- app/src/main/java/com/wrappedinplastic/theconverter/SettingsActivity.kt
+++ app/src/main/java/com/wrappedinplastic/theconverter/SettingsActivity.kt
@@ -1,8 +1,14 @@
 package com.wrappedinplastic.theconverter
 
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -17,12 +23,41 @@ class SettingsActivity : AppCompatActivity() {
 
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(),
+        OnSharedPreferenceChangeListener {
+        var sharedPreferences: SharedPreferences? = null
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity)
+        }
 
+        override fun onSharedPreferenceChanged(
+            sharedPreferences: SharedPreferences,
+            key: String
+        ) {
+            if (key == "theme_switch") {
+                val on = sharedPreferences.getBoolean("theme_switch", false)
+                if (on) {
+                    Toast.makeText(activity, "Night selected", Toast.LENGTH_SHORT).show()
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    Toast.makeText(activity, "Day mode selected", Toast.LENGTH_SHORT)
+                        .show()
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+        }
+
+        override fun onStart() {
+            super.onStart()
+            preferenceScreen.sharedPreferences
+                .registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onStop() {
+            super.onStop()
+            preferenceScreen.sharedPreferences
+                .unregisterOnSharedPreferenceChangeListener(this)
         }
     }
-
-
 }
